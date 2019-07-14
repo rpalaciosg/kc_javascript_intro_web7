ffe601b2a65e50d752bb0539086f8a35fdb465fb
Richard Palacios G <apalaciosg91@gmail.com>
2019-07-14 13:55:54 -0500

    Modifico kata 1 para que funcione con import y export.

---
+++ index.mjs
@@ -0,0 +1,26 @@
+import { FooBarQuix } from "./Primera_Kata/kata1.mjs";
+
+// Pruebas kata 1
+console.log('===============================================');
+console.log('=              Resultados Kata 1              =');
+console.log('===============================================');
+console.log(FooBarQuix(1));
+console.log(FooBarQuix(2));
+console.log(FooBarQuix(3));
+console.log(FooBarQuix(4));
+console.log(FooBarQuix(5));
+console.log(FooBarQuix(6));
+console.log(FooBarQuix(7));
+console.log(FooBarQuix(8));
+console.log(FooBarQuix(9));
+console.log(FooBarQuix(10));
+console.log(FooBarQuix(13));
+console.log(FooBarQuix(15));
+console.log(FooBarQuix(21));
+console.log(FooBarQuix(33));
+console.log(FooBarQuix(35));
+console.log(FooBarQuix(51));
+console.log(FooBarQuix(53));
+console.log(FooBarQuix(75));
+console.log(FooBarQuix(98));
+console.log(FooBarQuix(100));
